05fa939d56cc672eb2246b20668c4907d269688b
jahin44 <jahinhasan1@gmail.com>
2021-04-26 14:21:17 +0600

    properti and  auto Properti

---
+++ Console/CSharp-Features/Properti/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Properti
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var class1 = new Class1();
+            class1.FirstName = "jahin";
+            class1.LastName = "hasan";
+            var Name=class1.NameMethod();
+            Console.WriteLine(Name);
+        }
+    }
+}
