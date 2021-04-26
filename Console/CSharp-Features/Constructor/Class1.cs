d4484b8c156144f2ef717095060f4dc43f702af5
jahin44 <jahinhasan1@gmail.com>
2021-04-26 15:50:18 +0600

    Constructor

---
+++ Console/CSharp-Features/Constructor/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Constructor
+{
+    public class Class1
+    {
+        public string firstName;
+        public string lastName;
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public Class1(string FirstName)
+        {
+            firstName=FirstName;
+            Console.WriteLine(firstName);
+            
+        }
+        public Class1()
+        {
+           
+        Console.WriteLine(FirstName + " " + LastName);
+
+
+        }
+        public Class1(string FirstName,String LastName)
+        {
+
+            Console.WriteLine(FirstName + " " + LastName);
+
+        }
+    }
+}
