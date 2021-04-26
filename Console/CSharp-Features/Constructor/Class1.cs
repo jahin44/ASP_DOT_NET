21f1b07285202b739662dfa4cb1259c78f079c6b
jahin44 <jahinhasan1@gmail.com>
2021-04-26 20:58:07 +0600

    Association

--- Console/CSharp-Features/Constructor/Class1.cs
+++ Console/CSharp-Features/Constructor/Class1.cs
@@ -10,8 +10,8 @@ namespace Constructor
         public string lastName;
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public Class1(string FirstName)
-        {
+        public Class1(string FirstName):this()
+         {
             firstName=FirstName;
             Console.WriteLine(firstName);
             
@@ -19,15 +19,24 @@ namespace Constructor
         public Class1()
         {
            
-        Console.WriteLine(FirstName + " " + LastName);
+        Console.WriteLine(LastName);
 
 
         }
-        public Class1(string FirstName,String LastName)
+        public Class1(string FirstName,String LastName): this (FirstName)
         {
 
             Console.WriteLine(FirstName + " " + LastName);
 
         }
+
+
+        public void GetFullName()
+        {
+            firstName = FirstName;
+            lastName = LastName;
+            Console.WriteLine(firstName + " joy " + lastName);
+        }
+
     }
 }
--- Console/CSharp-Features/Constructor/Program.cs
+++ Console/CSharp-Features/Constructor/Program.cs
@@ -6,13 +6,15 @@ namespace Constructor
     {
         static void Main(string[] args)
         {
-            var class1 = new Class1("jahin");
+           // var class1 = new Class1("jahin");
 
             var class2 = new Class1("Israt", "supty");
-            var class3 = new Class1();
-            class3.FirstName = "Moon";
-            class3.LastName = "Hasan";
-            
+            //var class3 = new Class1();
+            //    class3.FirstName = "Moon";
+            //  class3.LastName = "Hasan";
+            class2.FirstName = "Jahin";
+            class2.LastName = "Chowdhury";
+            class2.GetFullName();
         }
     }
 }
