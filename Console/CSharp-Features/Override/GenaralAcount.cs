ae14ade75516c282b3091d96eb4ebe7e0ffe3924
jahin44 <jahinhasan1@gmail.com>
2021-04-28 11:24:32 +0600

    Override

---
+++ Console/CSharp-Features/Override/GenaralAcount.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Override
+{
+    public class GenaralAcount:BankAcount
+
+    {
+     private int interest { set; get; }
+
+     public override void withdrow(int amount)
+        {
+            if (amount <= Balance)
+            {
+                withdrow(2000); :base(BankAcount)
+            }
+
+        }
+     public int Interest(int balance)
+        {
+            var num= 0;
+           
+            if (balance/1000!=0)
+            {
+               num = balance / 1000;
+                interest = num;
+
+                return num * 10;
+            }
+            interest = num;
+
+            return num;
+
+        }
+
+
+        
+    }
+}
