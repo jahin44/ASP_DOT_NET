e09e46ebdac0b3bd5fa156ffd4bada215f290301
jahin44 <jahinhasan1@gmail.com>
2021-04-29 00:43:58 +0600

    constructor Chain Inheritance

--- Console/CSharp-Features/Override/Program.cs
+++ Console/CSharp-Features/Override/Program.cs
@@ -7,22 +7,22 @@ namespace Override
         static void Main(string[] args)
         {
             var genaralAcount = new GenaralAcount();
-            genaralAcount.Id = "G-101";
-            genaralAcount.Name = "jahin";
-            genaralAcount.AddTK(500000);
-            genaralAcount.Interest(genaralAcount.Balance);
-            genaralAcount.withdrow(20000);
-            genaralAcount.Interest(genaralAcount.Balance);
-            genaralAcount.CheckBalance();
+            //genaralAcount.Id = "G-101";
+            //genaralAcount.Name = "jahin";
+            //genaralAcount.AddTK(500000);
+            //genaralAcount.Interest(genaralAcount.Balance);
+            //genaralAcount.withdrow(20000);
+            //genaralAcount.Interest(genaralAcount.Balance);
+            //genaralAcount.CheckBalance();
 
             var specialAcount = new SpecialAcount();
-            specialAcount.Id = "S-102";
-            specialAcount.Name = "Supty";
-            specialAcount.AddTK(100000);
-            specialAcount.ServiceCharg(specialAcount.Balance);
-            specialAcount.withdrow(5000000);
-            specialAcount.ServiceCharg(specialAcount.Balance);
-            specialAcount.CheckBalance();
+            //specialAcount.Id = "S-102";
+            //specialAcount.Name = "Supty";
+            //specialAcount.AddTK(100000);
+            //specialAcount.ServiceCharg(specialAcount.Balance);
+            //specialAcount.withdrow(5000000);
+            //specialAcount.ServiceCharg(specialAcount.Balance);
+            //specialAcount.CheckBalance();
         }
     }
 }
