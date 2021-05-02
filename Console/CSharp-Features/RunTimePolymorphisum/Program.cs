01e1ccaa634706808101fb2a73dc0cf0acf5a25a
jahin44 <jahinhasan1@gmail.com>
2021-05-02 16:17:33 +0600

    Run Time Polymorphisum

---
+++ Console/CSharp-Features/RunTimePolymorphisum/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace RunTimePolymorphisum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var genaralAcount = new GenaralAcount("Jahin", "G-101");
+            genaralAcount.AddTK(500000);
+            //genaralAcount.Interest(genaralAcount.Balance);
+            //genaralAcount.withdrow(20000);
+            //genaralAcount.Interest(genaralAcount.Balance);
+            //genaralAcount.CheckBalance();
+
+            var specialAcount = new SpecialAcount("supty", "S-102");
+            specialAcount.AddTK(100000);
+            //specialAcount.ServiceCharg(specialAcount.Balance);
+            //specialAcount.withdrow(5000000);
+            //specialAcount.ServiceCharg(specialAcount.Balance);
+            //specialAcount.CheckBalance();
+
+            var Bank = new BankAcount();
+
+            List<BankAcount>account = new List<BankAcount>();
+
+
+
+        }
+    }
+}
