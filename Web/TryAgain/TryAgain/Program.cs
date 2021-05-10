04d0061ac85eceed4c5ea83baca76fe3b9958c8f
jahin44 <jahinhasan1@gmail.com>
2021-05-11 01:06:35 +0600

    try

--- Web/TryAgain/TryAgain/Program.cs
+++ Web/TryAgain/TryAgain/Program.cs
@@ -17,10 +17,10 @@ namespace TryAgain
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+             Host.CreateDefaultBuilder(args)
+                 .ConfigureWebHostDefaults(webBuilder =>
+                 {
+                     webBuilder.UseStartup<Startup>();
+                 });
     }
 }
