83eac045e72513a7fe554ffc1e1f89e70e64192d
jahin44 <jahinhasan1@gmail.com>
2021-05-24 14:58:32 +0600

    DependencyInjection

---
+++ Console/CSharp-Features/DependencyInjection/AccountManageService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyInjection
+{
+   public class AccountManageService
+    {
+        private IDatabaseAccess _databaseAccess;
+
+        public AccountManageService(IDatabaseAccess databaseAccess)
+        {
+            _databaseAccess = databaseAccess;
+        }
+         
+
+        public void CreateAccount(string email, string pass) 
+        {
+            if (!string.IsNullOrEmpty(email) && !string.IsNullOrEmpty(pass))
+            {
+
+
+                try
+                {
+                    var emailService = new EmailService();
+                    emailService.UseEmail(email);
+                }
+                catch
+                {
+                    _databaseAccess.DeleteAccount(email);
+                }
+             }
+            
+        }
+
+
+    }
+}
