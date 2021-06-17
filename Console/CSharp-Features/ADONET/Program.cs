21fe8259691bb8bb40194d286fee75e3f571e0eb
jahin44 <jahinhasan1@gmail.com>
2021-06-17 23:23:00 +0600

    ADONET insert only

---
+++ Console/CSharp-Features/ADONET/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace ADONET
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using SqlConnection connection = new SqlConnection();
+            connection.ConnectionString = "Server = DESKTOP-9UKEJ28\\SQLEXPRESS; Database = CSharpData; User Id = admin; Password = 123456;";
+            if (connection.State == System.Data.ConnectionState.Closed)
+            {
+                connection.Open();
+
+            }
+
+            using SqlCommand command = new SqlCommand();
+            var sql = "insert into Student (Name, Weight) values('Supty', 70)";
+            command.Connection = connection;
+
+            WriteOperation(sql, connection);
+            
+            Console.WriteLine("insert completed");
+        }
+        static void WriteOperation(string sql, SqlConnection connection)
+        {
+            if (connection.State == System.Data.ConnectionState.Closed)
+            {
+                connection.Open();
+
+            }
+
+            using SqlCommand command = new SqlCommand();
+            command.CommandText = sql;
+            command.Connection = connection;
+
+            command.ExecuteNonQuery();
+             
+
+        }
+
+
+
+    }
+}
