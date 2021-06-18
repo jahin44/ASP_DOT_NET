43bed21acda01519d8e9b5cb52b142b556abd618
jahin44 <jahinhasan1@gmail.com>
2021-06-19 03:21:21 +0600

    Ado read data

--- Console/CSharp-Features/ADONET/Program.cs
+++ Console/CSharp-Features/ADONET/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Data.SqlClient;
 
 namespace ADONET
@@ -16,10 +17,17 @@ namespace ADONET
             }
 
             using SqlCommand command = new SqlCommand();
-            var sql = "insert into Student (Name, Weight) values('hasan', 80)";
+            var sql = "UPDATE Student SET Name ='jahin', weight = '81' WHERE Id = 4 ";
+
             command.Connection = connection;
 
-            WriteOperation(sql, connection);
+          //  WriteOperation(sql, connection);
+            var sql2 = "select * from student";
+            var students = ReadOperation(sql2, connection);
+            foreach(var student in students)
+            {
+                Console.WriteLine($"Id ={student.Id},Name= {student.Name},Weight ={student.Weight}");
+            }
             
             Console.WriteLine("insert completed");
         }
@@ -36,8 +44,34 @@ namespace ADONET
             command.Connection = connection;
 
             command.ExecuteNonQuery();
-             
+        }
 
+        public class Student
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public decimal Weight { get; set; }
+             
+        }
+
+        static IList<Student> ReadOperation (string sql, SqlConnection connection)
+        {
+            if (connection.State == System.Data.ConnectionState.Closed)
+            {
+                connection.Open();
+            }
+            using SqlCommand command = new SqlCommand(sql, connection);
+            var reader = command.ExecuteReader();
+            var students = new List<Student>();
+            while (reader.Read())
+            {
+                var student = new Student();
+                student.Id = (int)reader["Id"];
+                student.Name = (string)reader["Name"];
+                student.Weight = (decimal)reader["Weight"];
+                students.Add(student);
+            }
+            return students;
         }
 
 
