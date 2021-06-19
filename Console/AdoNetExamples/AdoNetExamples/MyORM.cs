79fc5fed9b64f367fdca22ca0aeace9ee792b154
jahin44 <jahinhasan1@gmail.com>
2021-06-20 02:33:27 +0600

    Assignment 2 ORM Insert done

---
+++ Console/AdoNetExamples/AdoNetExamples/MyORM.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdoNetExamples
+{
+    public class MyORM<T> where T : class
+    {
+        private SqlConnection _sqlConnection;
+
+        public MyORM(SqlConnection connection)
+        {
+            _sqlConnection = connection;
+        }
+
+        public MyORM(string connectionString)
+            : this(new SqlConnection(connectionString))
+        {
+
+        }
+
+        public void Update(T item)
+        {
+
+        }
+
+        public void Insert(T item)
+        {
+            var sql = new StringBuilder("Insert into ");
+            var type = item.GetType();
+            var properties = type.GetProperties();
+
+            sql.Append(type.Name);
+            sql.Append('(');
+            foreach (var property in properties)
+            {
+                sql.Append(' ').Append(property.Name).Append(',');
+            }
+            sql.Remove(sql.Length - 1, 1);
+
+            sql.Append(") values(");
+
+            foreach (var property in properties)
+            {
+                sql.Append('@').Append(property.Name).Append(',');
+            }
+            sql.Remove(sql.Length - 1, 1);
+            sql.Append(");");
+
+            var query = sql.ToString();
+
+            var command = new SqlCommand(query, _sqlConnection);
+            foreach (var property in properties)
+            {
+                command.Parameters.AddWithValue(property.Name,property.GetValue(item));
+            }
+
+            if (_sqlConnection.State == System.Data.ConnectionState.Closed)
+                _sqlConnection.Open();
+
+            command.Connection = _sqlConnection;
+            command.ExecuteNonQuery();
+            Console.WriteLine("Insert Successfull");
+        }
+
+     /*   public void Delete(T item)
+        {
+            Delete(item.Id);
+        }
+
+        public void Delete(int id)
+        {
+
+        }*/
+
+        public IList<T> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
--- Console/AdoNetExamples/AdoNetExamples/Program.cs
+++ Console/AdoNetExamples/AdoNetExamples/Program.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Data.SqlClient;
 
-namespace ADONET
+namespace AdoNetExamples
 {
     class Program
     {
@@ -10,35 +10,36 @@ namespace ADONET
         {
             using SqlConnection connection = new SqlConnection();
             connection.ConnectionString = "Server = DESKTOP-9UKEJ28\\SQLEXPRESS; Database = CSharpData; User Id = admin; Password = 123456;";
-            if (connection.State == System.Data.ConnectionState.Closed)
-            {
-                connection.Open();
+            var sql = "insert into student (name, weight) values('jalaluddin', 23)";
+            //WriteOperation(sql, connection);
 
-            }
+            Student student = new Student();
+            MyORM<Student> ORM = new MyORM<Student>(connection.ConnectionString);
+            student.Name ="Supty" ;
+            student.Weight = 75;
+            
 
-            using SqlCommand command = new SqlCommand();
-            var sql = "insert into student(Id,Name,Weight) values('','Jahin',30)";
-            //var sql2 = "update student set Id=6,Name=joy where weight=20.00";
+            ORM.Insert(student);
 
-            command.Connection = connection;
 
-            WriteOperation(sql, connection); ;
-           // var sql2 = "select * from student";
+
+
+
+            var sql2 = "select * from student";
            // var students = ReadOperation(sql2, connection);
+
            /* foreach(var student in students)
             {
-                Console.WriteLine($"Id ={student.Id},Name= {student.Name},Weight ={student.Weight}");
-            }*/
-            
-            Console.WriteLine("insert completed");
+                Console.WriteLine($"Id = {student.Id}, Name = {student.Name}, Weight = {student.Weight}");
+            }
+
+            Console.WriteLine("Complete");*/
         }
-        static void WriteOperation(string sql, SqlConnection connection)
+
+        /*static void WriteOperation(string sql, SqlConnection connection)
         {
             if (connection.State == System.Data.ConnectionState.Closed)
-            {
                 connection.Open();
-
-            }
 
             using SqlCommand command = new SqlCommand();
             command.CommandText = sql;
@@ -47,35 +48,29 @@ namespace ADONET
             command.ExecuteNonQuery();
         }
 
-        public class Student
-        {
-            public int Id { get; set; }
-            public string Name { get; set; }
-            public decimal Weight { get; set; }
-             
-        }
+       
 
-        static IList<Student> ReadOperation (string sql, SqlConnection connection)
+        static IList<Student> ReadOperation(string sql, SqlConnection connection)
         {
             if (connection.State == System.Data.ConnectionState.Closed)
-            {
                 connection.Open();
-            }
+
             using SqlCommand command = new SqlCommand(sql, connection);
+
             var reader = command.ExecuteReader();
+
             var students = new List<Student>();
-            while (reader.Read())
+            while(reader.Read())
             {
                 var student = new Student();
                 student.Id = (int)reader["Id"];
                 student.Name = (string)reader["Name"];
                 student.Weight = (decimal)reader["Weight"];
+
                 students.Add(student);
             }
+
             return students;
-        }
-
-
-
+        }*/
     }
 }
