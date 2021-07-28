02de516d3237dc161d9d87ebfbf6b22c493d4e27
jahin44 <jahinhasan1@gmail.com>
2021-07-28 12:30:30 +0600

    migrations problem

---
+++ Web/WebApp/WebAppMVC/Data/Migrations/20210727131429_AddedStudentIdCourseId.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebAppMVC.Migrations.Training
+{
+    public partial class AddedStudentIdCourseId : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
